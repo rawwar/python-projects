611758ad4a08832da5aa894e0a591e4087e24f7a
kalyan <kalyan.ben10@live.com>
2019-05-04 18:52:09 +0530

    Updated Doc strings and code to pep8 standard

--- Lss/lss/lss.py
+++ Lss/lss/lss.py
@@ -10,14 +10,13 @@ data_folder = "data"
 
 def group_by_hash(file_list):
     '''
-    This function takes a list and groups elements 
+    This function takes a list and groups elements
     based on a hash and returns a dictionary.
 
     Parameters:
         file_list: list of all file names
-    
     Returns:
-        grouped_dict: A dictionary where key is a hash and value is a 
+        grouped_dict: A dictionary where key is a hash and value is a
                       list of file names with similar pattern
     '''
     grouped_dict = defaultdict(lambda: [])
@@ -50,9 +49,12 @@ def check_pattern(file1, file2):
 
 
 def parse_simple_pattern(lst):
-    # global global_lst
-    temp_str = ""
-    pattern = ""
+    '''
+    This function takes a list of similar file patterns,
+    parses them and  passes them to print_pattern
+    '''
+    sequence = ""
+    file_pattern = ""
     count = 1
     for each in lst:
         if isinstance(each, list):
@@ -60,22 +62,25 @@ def parse_simple_pattern(lst):
             group_lst = [
                 list(group) for group in consecutive_groups(sorted(int_lst))
             ]
-            pattern = ",".join(
+            # makes a sequence separated by comma
+            sequence = ",".join(
                 str(i) if len(i) == 1 else str(i[0]) + "-" + str(i[-1])
                 for i in group_lst)
+            # To check if the numbers are zero padded
             pad = True if each[0].startswith("0") else False
-            temp_str += f"%{0 if pad else ''}{len(each[0]) if len(each[0])>1 else ''}d"
+            temp = len(each[0])
+            # Forming file pattern
+            file_pattern += f"%{0 if pad else ''}{temp if temp>1 else ''}d"
             count = len(int_lst)
         else:
-            temp_str += each
-
-    print_patterns([temp_str, pattern, count])
+            file_pattern += each
+    print_pattern([count, file_pattern, sequence])
 
 
 def combine_values(lst):
     '''
-    This function takes a list of file patterns and combines them to be parsable by
-    parse_simple_pattern function.
+    This function takes a list of file patterns and combines
+    them to be parsable by parse_simple_pattern function.
     '''
     fin_lst = []
     for each in zip(*lst):
@@ -85,10 +90,15 @@ def combine_values(lst):
 
 
 def parse_for_patterns(pattern_wise_dict):
+    '''
+    This function takes a dictionary and parses it to separate patterns in
+    each of the value and then passes each of these separated values to
+    combine values function.
+    '''
     for key, value in pattern_wise_dict.items():
         split_index = []
         if len(value) <= 1:
-            print_patterns([value[0][0], None])
+            print_pattern([value[0][0], None])
             continue
         for index, (i, j) in enumerate(pairwise(value)):
             if check_pattern(i, j):
@@ -102,13 +112,13 @@ def parse_for_patterns(pattern_wise_dict):
             combine_values(value[i:j])
 
 
-def print_patterns(file_pattern_lst):
+def print_pattern(file_pattern_lst):
     '''
     This function takes a list and prints it to console in the following format
     Number_of_files    File Pattern    Series.
     '''
     if file_pattern_lst[1]:
-        print(file_pattern_lst[2], file_pattern_lst[0], file_pattern_lst[1])
+        print(file_pattern_lst[0], file_pattern_lst[1], file_pattern_lst[2])
     else:
         print(1, file_pattern_lst[0])
 
