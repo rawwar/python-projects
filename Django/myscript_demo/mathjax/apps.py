d3c311592ea9e4375a391befbcf4a828a1b8addd
Kalyan reddy kasireddy <20266953+rawwar@users.noreply.github.com>
2021-04-04 09:51:10 +0530

    Delete Django directory

--- Django/myscript_demo/mathjax/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MathjaxConfig(AppConfig):
-    name = 'mathjax'
--- Django/password_generator_website/generator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import password_generate, show_password
-
-urlpatterns = [
-    path('',password_generate,name='generate'),
-    path('show/', show_password, name='show_password')
-]
